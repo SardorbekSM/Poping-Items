c9aaaee998fdd38fef1033bb3b0d5898006d8922
Sardorbek <50486001+SardorbekSM@users.noreply.github.com>
2022-02-11 08:45:03 +0500

    Revert "Launcher method changed"
    This reverts commit b92bd42c855aa7e277ef393d853faa1725e2f3ac.

---
+++ Assets/Scripts/Core/Launcher.cs
@@ -0,0 +1,20 @@
+﻿using Control;
+using VContainer.Unity;
+
+namespace Core
+{
+    public class Launcher : IStartable
+    {
+        private readonly GameProcessControl _gameProcessControl;
+
+        public Launcher(GameProcessControl gameProcessControl)
+        {
+            _gameProcessControl = gameProcessControl;
+        }
+
+        public void Start()
+        {
+            _gameProcessControl.BeginSpawn();
+        }
+    }
+}
