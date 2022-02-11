eeccb6f9e555231afc0ad5f06071742c31367070
Sardorbek <50486001+SardorbekSM@users.noreply.github.com>
2022-02-11 08:44:48 +0500

    Revert "Deleted"
    This reverts commit 8f1677bd903efe8ffd9634b3450906ee3e575f63.

---
+++ Assets/Scripts/Control/GameProcessControl.cs
@@ -0,0 +1,41 @@
+﻿using Control.Interfaces;
+using Core.Spawner;
+using Core.WaiterAsync;
+using Model;
+using UnityEngine;
+using VContainer.Unity;
+
+namespace Control
+{
+    public class GameProcessControl
+    {
+        private readonly SpawnModel _spawnModel;
+        private readonly SpawnerWithPool _spawnerWithPool;
+        private LoopedActionAsync _loopedActionAsync;
+
+        public GameProcessControl(SpawnerWithPool spawnerWithPool, SpawnModel spawnModel)
+        {
+            _spawnerWithPool = spawnerWithPool;
+            _spawnModel = spawnModel;
+        }
+
+        public void BeginSpawn()
+        {
+            _spawnerWithPool.Dispose();
+            _loopedActionAsync = new LoopedActionAsync();
+            _loopedActionAsync.DoAction += _spawnerWithPool.Spawn;
+            StopSpawn();
+            _loopedActionAsync.Begin(_spawnModel.SpawnDuration);
+        }
+
+        public void StopSpawn()
+        {
+            _loopedActionAsync.EndLoop();
+        }
+
+        public void OnWin()
+        {
+            Debug.Log("You Win!");
+        }
+    }
+}
