ce499a5ec4e3ae948d436e7c1a3231bef3cbde19
Sardorbek <esemxauz@gmail.com>
2022-02-22 10:16:39 +0500

    Commit deleted

--- Assets/Scripts/Control/Interfaces/ILevelController.cs
+++ Assets/Scripts/Control/Interfaces/ILevelController.cs
@@ -3,7 +3,7 @@
 namespace Control.Interfaces
 {
     public interface ILevelController : IControl
-    {// Для других скриптов тоже создать интерфейсы?
+    {
         event Action Scored;
         float AddScore();
     }
